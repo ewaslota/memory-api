1dc2c7aaa5db9eea24a7a0a1b2704842b9758d7b
Sebastian Słomian <slomian@wszib.edu.pl>
2024-06-16 09:05:28 +0200

    Usuwanie fiszek
    (cherry picked from commit 77eedb75a1c839bf93c2a32332f3da3e1d2d100c)

--- src/main/java/pl/wszib/memoryapi/services/CardService.java
+++ src/main/java/pl/wszib/memoryapi/services/CardService.java
@@ -47,4 +47,13 @@ public class CardService {
                 .map(c -> new CardResponse(categoryId, c))
                 .toList();
     }
+
+    @Transactional
+    public void removeCard(Long categoryId, Long cardId) {
+//        cardRepository.findByIdAndCategoryId(cardId, categoryId)
+//                .ifPresent(cardRepository::delete);
+
+        categoryRepository.findById(categoryId)
+                .ifPresent(category -> category.removeCard(cardId));
+    }
 }
